7d80bd57b63befc8f0f2136812a3707cd0d8913a
Bryce Nordgren <bnordgren@gmail.com>
2012-02-26 00:25:12 +0100

    factored out the appliance specific configuration

---
+++ appliance-configuration.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ...  } : 
+{
+  # specify the kernel
+  boot.kernelPackages = pkgs.linuxPackages_3_2 ; 
+
+  # customize the choice of packages for this appliance
+  nixpkgs.config = {
+    packageOverrides = orig : {
+      postgresql = pkgs.postgresql90;
+    };
+  };
+
+  #
+  # List services that you want to enable:
+  #
+
+  # Add an OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Add the NixOS Manual on virtual console 8
+  services.nixosManual.showManual = false;
+
+  # turn on postgresql with the postgis extension
+  services.postgresql = {
+    enable = true ;
+    enableTCPIP = true ; 
+    authentication = ''
+     # generated file, do not edit!
+     local all all trust
+     host  all all 127.0.0.1/32 md5
+     host  all all 0.0.0.0/0    md5
+    ''; 
+    extraPlugins = [ pkgs.postgis.v_2_0_0a4 ] ;
+  } ; 
+}
