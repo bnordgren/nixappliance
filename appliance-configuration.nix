76aeb10308033b83d1c3d18d7596aa67e45d15e6
Bryce Nordgren <bnordgren@gmail.com>
2012-02-26 00:34:48 +0100

    added the basic default services to the base appliance.

---
+++ appliance-configuration.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ...  } : 
+{
+  # specify the kernel
+  boot.kernelPackages = pkgs.linuxPackages_3_2 ; 
+
+  # customize the choice of packages for this appliance
+  #nixpkgs.config = {
+  #  packageOverrides = orig : {
+  #  };
+  #};
+
+  #
+  # List services that you want to enable:
+  #
+
+  # Add an OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Add the NixOS Manual on virtual console 8
+  services.nixosManual.showManual = false;
+}
