d8d99bab0232a96668874c4c326f54bacce63f7f
Bryce Nordgren <bnordgren@gmail.com>
2012-09-18 00:46:18 +0200

    updated OS and added NFS server

--- appliance-configuration.nix
+++ appliance-configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ...  } : 
 {
   # specify the kernel
-  boot.kernelPackages = pkgs.linuxPackages_2_6_39 ; 
+  boot.kernelPackages = pkgs.linuxPackages_3_2 ; 
 
   # customize the choice of packages for this appliance
   #nixpkgs.config = {
@@ -18,4 +18,10 @@
 
   # Add the NixOS Manual on virtual console 8
   services.nixosManual.showManual = false;
+
+  # Add an NFS export to the rxdata front end.
+  services.nfs.server = { 
+    enable = true ; 
+    exports = ''/mnt/rxcadre 192.168.56.2(rw,all_squash,no_subtree_check)'' ; 
+  } ; 
 }
